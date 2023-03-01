3a711e6024057c75e8e4b2ad56799d27364d4641
Fauzan-affan <fauzanaffanzaki93@gmail.com>
2023-03-01 21:27:01 +0700

    deploy staging

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -44,22 +44,6 @@ const Dashboard = () => {
     setIsUlasanTerkirim(true);
   };
 
-  // const ratingStars = {
-  //   size: 50,
-  //   count: 10,
-  //   color: "black",
-  //   activeColor: "red",
-  //   value: 7.5,
-  //   a11y: true,
-  //   isHalf: true,
-  //   emptyIcon: <i className="far fa-star" />,
-  //   halfIcon: <i className="fa fa-star-half-alt" />,
-  //   filledIcon: <i className="fa fa-star" />,
-  //   onChange: (newValue) => {
-  //     console.log(`Example 2: new value is ${newValue}`);
-  //   },
-  // };
-
   useEffect(() => {
     Cookies.get("role").length > 0 && setRole(Cookies.get("role"));
   }, [role]);
