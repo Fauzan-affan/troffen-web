356ec12203d4140d780429c14288fc7d8e076643
Fauzan-affan <fauzanaffanzaki93@gmail.com>
2023-04-02 18:34:06 +0700

    fixing step 1.1

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import NextNProgress from "nextjs-progressbar";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   const getLayout = Component.getLayout || ((page) => page);
   return (
-    <SessionProvider session={session}>
-      <NextNProgress />
-      {getLayout(<Component {...pageProps} />)}
-    </SessionProvider>
+    <ErrorBoundary>
+      <SessionProvider session={session}>
+        <NextNProgress />
+        {getLayout(<Component {...pageProps} />)}
+      </SessionProvider>
+    </ErrorBoundary>
   );
 }
